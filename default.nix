72732060ceaa433c0f20aeac88f06c4153bc2acf
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-01-11 15:38:39 -0500

    default.nix: don't require `pkgs`

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 # If you're using npins, do `adiosPath = sources.adios.outPath;
 # If you're using flakes, don't worry about it - this file will be called
 # automaticlaly
-{ pkgs, adiosPath }:
+{ adiosPath }:
 
 let
   adios = (import "${adiosPath}/default.nix").adios;
@@ -19,20 +19,5 @@ let
       };
     };
   };
-
-  root = {
-    name = "root";
-    modules = importModules ./modules;
-  };
-
-  tree = (adios root).eval {
-    options = {
-      "/nixpkgs" = {
-        inherit pkgs;
-      };
-    };
-  };
 in
-# We have each wrapper `foo` point to all its options, so you can do
-# `(import ./wrappers {}).foo.some-option`
-tree.root.modules
+importModules ./modules
