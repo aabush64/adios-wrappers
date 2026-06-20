3fba6b25ad678663a077115fe9c6452f72c1a90b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-06-20 00:29:42 -0400

    default.nix: fix adios warning for derivations

--- default.nix
+++ default.nix
@@ -3,9 +3,7 @@
 let
   warn = builtins.warn or builtins.trace;
   adios =
-    if builtins.isAttrs args.adios then
-    args.adios
-    else
+    if builtins.isPath args.adios || args.adios ? outPath then
       warn ''
         The adios-wrappers 'default.nix' now expects to be passed an instantiated `adios` function, rather than the source.
 
@@ -18,9 +16,9 @@ let
         ```
           adios = import sources.adios;
           adios-wrappers = import sources.adios-wrappers { inherit adios; }
-        ```
-
-      '' (import args.adios);
+        ```'' (import args.adios)
+    else
+      args.adios;
 in
 
 adios.lib.importModules ./modules
