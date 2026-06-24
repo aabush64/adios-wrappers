a732ac847ec6fabfbc0672652a7a94de1938b080
ABush <69372251+aabush64@users.noreply.github.com>
2026-06-24 17:06:45 -0600

    modules/themeJet: init

---
+++ modules/themeJet/conversions.nix
@@ -0,0 +1,60 @@
+{lib}: let
+  inherit (builtins) mapAttrs concatStringsSep substring;
+  inherit (lib.attrsets) mapAttrs' nameValuePair;
+  hexDecDigits = {
+    "0" = 0;
+    "1" = 1;
+    "2" = 2;
+    "3" = 3;
+    "4" = 4;
+    "5" = 5;
+    "6" = 6;
+    "7" = 7;
+    "8" = 8;
+    "9" = 9;
+    A = 10;
+    B = 11;
+    C = 12;
+    D = 13;
+    E = 14;
+    F = 15;
+  };
+
+  hexRgbMath = hex:
+    (16 * hexDecDigits."${substring 0 1 hex}"
+    + hexDecDigits."${substring 1 1 hex}");
+  
+  # Gives us hexDecDigits but with digits as key and hex as value
+  decHexDigits = mapAttrs'
+    (name: value: nameValuePair
+      (toString value) (toString name))
+    hexDecDigits;
+
+  rgbHexMath = value:
+    map (x: decHexDigits.${toString x})
+      [ (value / 16) (value - (value / 16) * 16) ];
+
+in {
+  # Converts an rgb attrset to hex string
+  # rgb = { r = 24; g = 24; b = 24; }; => "#242424"
+  rgbToHex = rgb_attrs:
+    (x: "#${x.r}${x.g}${x.b}")
+    (mapAttrs
+      (name: value: concatStringsSep ""
+        (rgbHexMath value))
+      (rgb_attrs));
+
+
+  # Todo: validate that hex is not missing '#'
+  hexToRgb = hex_string:
+    (mapAttrs
+      (name: value:
+        hexRgbMath value)
+    )
+    {
+      r = substring 1 2 hex_string;
+      g = substring 3 2 hex_string;
+      b = substring 5 2 hex_string;
+    };
+
+}
---
+++ modules/themeJet/default.nix
@@ -0,0 +1,85 @@
+{types, ...} @ adios: {
+  inputs = {
+    nixpkgs.from = {parent}: parent.nixpkgs;
+  };
+
+  options = {
+    theme = {
+      # Generalized theme file schema
+      # {
+      #   fg1.hex = "#282828"
+      #   fg2.rgb = {
+      #     r = 24; g = 24; b = 24;
+      #   }
+      # }
+      type =
+        types.attrsOf
+        ((types.struct "themeStruct" {
+          HEX = types.string;
+          rgb = types.attrsOf types.int;
+        }).override {
+          total = false;
+          unknown = false;
+        });
+      # Color Values we can Translate
+      # RGB - {r = 23; g = 24; b = 25;};
+      # HEX - "#FBCC12";
+      # HSL -- Lets wait to add this for now
+      # HSV -- Lets wait to add this for now
+      # OKCLH -- Lets wait to add this for now
+      mutatorType = types.attrs;
+      mergeFunc = adios.lib.merge.attrs.recursively;
+    };
+  };
+
+  impl = {
+    options,
+    inputs,
+  }: let
+    inherit (inputs.nixpkgs.lib) toLower toUpper;
+    inherit (import ./conversions.nix {inherit (inputs.nixpkgs) lib;}) rgbToHex hexToRgb;
+    inherit (builtins) mapAttrs concatStringsSep attrValues;
+
+    genTheme = themeSet:
+      mapAttrs (
+        name: value:
+          (
+            if value ? RGB
+            then {rgb = value.RGB;}
+            else if value ? HEX
+            then {rgb = hexToRgb (toUpper value.HEX);}
+            else throw "You done fucked up"
+          )
+          // (
+            if value ? HEX
+            then {hex = value.HEX;}
+            else if value ? RGB
+            then {hex = rgbToHex value.RGB;}
+            else throw "You done fucked up"
+          )
+      )
+      themeSet;
+  in
+    assert !(options ? themeFile && options ? theme); rec {
+      theme = rec {
+        keys = (
+          if options ? theme
+          then genTheme options.theme
+          else throw "How? Why? Porque?"
+        );
+
+        # Prints out hex string in standard all caps
+        hex = mapAttrs (name: value: value.hex) keys;
+        hexLow = mapAttrs (name: value: toLower value.hex) keys;
+
+        rgb = mapAttrs (name: value:
+          concatStringsSep "," (map toString (attrValues value.rgb)))
+        keys;
+        rgbList = mapAttrs (name: value:
+          attrValues value.rgb)
+        keys;
+      };
+
+      themeColors = theme.keys;
+    };
+}
